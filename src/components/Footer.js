5bddce460e06b0c09beac7d35ff3b4e0eb76459d
moabmo <monyorodemongare@gmail.com>
2024-09-14 22:08:22 +0300

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,6 +15,16 @@ import "../styles/Footer.css";
 const Footer = () => {
   return (
     <footer>
+      {/* Map section */}
+      <div className="footer-map">
+        <iframe
+          title="Location Map"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d63815.92559077185!2d36.9194273!3d-1.4755306!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f9ff8b67fcb47%3A0xe1d3fa8772611820!2sLonestar%20Dental%20Care!5e0!3m2!1sen!2ske!4v1726372949180!5m2!1sen!2ske"
+          allowFullScreen
+          loading="lazy"
+        ></iframe>
+      </div>
+
       <div className="footer-content">
         <address>
           <div className="icon-text">
@@ -30,6 +40,7 @@ const Footer = () => {
             <p>info@lonestardentalcare.com</p>
           </div>
         </address>
+
         <div className="social-links">
           <a
             href="https://www.facebook.com/lonestardentalcare"
@@ -52,7 +63,7 @@ const Footer = () => {
           >
             <FontAwesomeIcon icon={faInstagram} />
           </a>
-        </div>{" "}
+        </div>
         <br />
         <p>
           © 2024 <span style={{ color: "#FFD700" }}>|</span> Lonestar Dental
--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,20 +1,46 @@
-// ContactUs.js
 import React from 'react';
 import '../styles/ContactUs.css';
 
 const ContactUs = () => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const formData = new FormData(form);
+
+    try {
+      const response = await fetch('https://docs.google.com/forms/d/e/1FAIpQLScg1rO7rFBRqKIgzGuQ5ajUo4chJZXHJk2ol6xYeG3SvfYm4g/formResponse', {
+        method: 'POST',
+        body: formData,
+        mode: 'no-cors',  // Required to handle CORS
+      });
+
+      // Since CORS restrictions might prevent checking response status
+      alert('Message sent successfully!');
+      
+      // Clear the form fields
+      form.reset();
+    } catch (error) {
+      alert('Error: ' + error.message);
+    }
+  };
+
   return (
     <div className="contact-us">
       <h2>Contact Us</h2>
-      <form>
+      <form id="custom-contact-form" onSubmit={handleSubmit}>
         <label>Name</label>
-        <input type="text" name="name" />
+        <input type="text" name="entry.2005620554" required />
+        
         <label>Email</label>
-        <input type="email" name="email" />
+        <input type="email" name="entry.1045781291" required />
+        
         <label>Phone</label>
-        <input type="text" name="phone" />
+        <input type="text" name="entry.1166974658" />
+        
         <label>Message</label>
-        <textarea name="message"></textarea>
+        <textarea name="entry.839337160" required></textarea>
+        
         <button type="submit">Send</button>
       </form>
       <div className="contact-info">
